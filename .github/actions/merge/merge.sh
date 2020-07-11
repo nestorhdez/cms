651cda863ff413a71cdefcf007a15e8acafec272
Nestor hdez <nestorhdez.26@gmail.com>
2020-07-11 01:33:55 +0100

    Add merge logic and echo git status

--- .github/actions/merge/merge.sh
+++ .github/actions/merge/merge.sh
@@ -1,4 +1,20 @@
-#!/bin/sh
-set -e
+# !/bin/sh
 
-[[ -e ./index.html ]] && echo "Index exists. Correct directory"
+INDEX=1
+TOTAL_FILES="$(find ./posts -maxdepth 1 -type f | wc -l)"
+OUTPUT="./posts.json"
+
+echo "{"$'\n'\"posts\"": [" > $OUTPUT
+
+for file in ./posts/*.json; do
+
+  cat $file >> $OUTPUT
+
+  if [[ INDEX -lt TOTAL_FILES ]]; then
+    echo "," >> $OUTPUT
+    ((INDEX++))
+  fi
+  
+done
+
+echo $'\n'"]"$'\n'"}" >> $OUTPUT
